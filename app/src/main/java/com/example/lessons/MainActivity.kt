11b19190fb38efe4987ade39605bd9d06da0fb78
данил <danil.dengin.2000@mail.ru>
2022-09-18 00:15:48 +0300

    task

--- app/src/main/java/com/example/lessons/MainActivity.kt
+++ app/src/main/java/com/example/lessons/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.lessons
 
-import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val txt: TextView=findViewById(R.id.contact1)
+        txt.setOnClickListener() {
+            this.switch()
+        }
+    }
+
+    private fun switch() {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction
+            .replace(R.id.fragment_container, FragmentDetails.newInstance(R.id.contact1.toString()))
+            .addToBackStack(null)
+            .commit()
     }
 }
+
